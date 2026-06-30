999541c5251625831dd3ab678e962c1732ed22da
RubenJansPXL <12501270@student.pxl.be>
2026-06-30 15:35:26 +0200

    tekst veranderd

--- eindreflectie.js
+++ eindreflectie.js
@@ -1,7 +1,7 @@
 function downloadReflectie() {
     // Create a simple text file with reflection content
     const content = "EINDREFLECTIE\n\n" +
-        "Rol 1:\nJouw reflectie hier...\n\n" +
+        "Rol 1:\nAls creatieve vormgever ontwerp ik beelden die niet alleen esthetisch zijn, maar ook functioneel. Met vormen, kleuren en compositie geef ik ideeën vorm tot designs die de aandacht trekken en de gebruiker begeleiden. Of het nu gaat om een logo of een complete interface, ik werk tot in detail om elk ontwerp kloppend te maken.\n\n" +
         "Rol 2:\nJouw reflectie hier...\n\n" +
         "Rol 3:\nJouw reflectie hier...\n\n" +
         "Rol 4:\nJouw reflectie hier...\n";
